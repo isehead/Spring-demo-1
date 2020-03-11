222ed9e3965ffc77edb75dd88a179102c36d3db5
isehead <isehead@gmail.com>
2020-03-11 17:55:14 +0500

    Setter injection

--- src/com/springdemo/FootballCoach.java
+++ src/com/springdemo/FootballCoach.java
@@ -2,6 +2,17 @@ package com.springdemo;
 
 public class FootballCoach implements Coach {
 
+    private FortuneService fortuneService;
+
+    public FootballCoach() {
+        System.out.println("Inside no-arg constructor");
+    }
+
+    public void setFortuneService(FortuneService fortuneService) {
+        System.out.println("Inside setter");
+        this.fortuneService = fortuneService;
+    }
+
     @Override
     public String getDailyWorkout() {
         return ("Strike 20 penalty kicks on target");
@@ -9,6 +20,6 @@ public class FootballCoach implements Coach {
 
     @Override
     public String getDailyFortune() {
-        return null;
+        return fortuneService.getFortune();
     }
 }
---
+++ src/com/springdemo/SetterDemoApp.java
@@ -0,0 +1,22 @@
+package com.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SetterDemoApp {
+
+    public static void main(String[] args) {
+
+        //load the Spring configuration file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        //retrieve bean from Spring container
+        FootballCoach theCoach = context.getBean("fbCoach", FootballCoach.class);
+
+        //call methods on the bean
+        System.out.println(theCoach.getDailyWorkout());
+        System.out.println(theCoach.getDailyFortune());
+
+        //close the context
+        context.close();
+    }
+}
